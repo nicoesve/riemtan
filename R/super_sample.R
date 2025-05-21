a906bdfa585f47596c328ff91a90c7c386a4118f
NE <nescoba@iu.edu>
2025-05-21 15:14:28 +0000

    Enhance documentation for CSuperSample class methods and active bindings

--- R/super_sample.R
+++ R/super_sample.R
@@ -1,3 +1,8 @@
+#' CSuperSample Class
+#'
+#' @description
+#' This class represents a collection of CSample objects (samples of connectomes), providing methods to aggregate, analyze, and compute statistics across multiple samples sharing the same Riemannian metric.
+#' @export
 CSuperSample <- R6::R6Class(
   classname = "CSuperSample",
   private = list(
@@ -5,6 +10,10 @@ CSuperSample <- R6::R6Class(
     s_cov = NULL, gathered_sample = NULL, f_mean = NULL, W = NULL, T = NULL
   ),
   public = list(
+    #' @description Initialize a CSuperSample object
+    #'
+    #' @param samples A list of CSample objects. All must use the same Riemannian metric.
+    #' @return A new `CSuperSample` object.
     initialize = function(samples) {
       if (!is.list(samples)) stop("samples must be a list.")
 
@@ -39,16 +48,28 @@ CSuperSample <- R6::R6Class(
       private$W <- NULL
       private$T <- NULL
     },
+
+    #' @description Compute the total variation of the aggregated sample.
+    #'
+    #' @return None. This function is called for its side effects. The result is stored in the `variation` active binding.
     compute_variation = function() {
       if (self$full_sample |> is.null()) self$gather()
       self$full_sample$compute_variation()
       private$var <- self$full_sample$variation
     },
+
+    #' @description Compute the sample covariance matrix of the aggregated sample.
+    #'
+    #' @return None. This function is called for its side effects. The result is stored in the `sample_cov` active binding.
     compute_sample_cov = function() {
       if (self$full_sample |> is.null()) self$gather()
       self$full_sample$compute_sample_cov()
       private$s_cov <- self$full_sample$sample_cov
     },
+
+    #' @description Gather all connectomes from the list of samples into a single CSample object.
+    #'
+    #' @return None. This function is called for its side effects. The result is stored in the `full_sample` active binding.
     gather = function() {
       if (private$samples |> is.null()) stop("samples must be specified.")
 
@@ -85,12 +106,20 @@ CSuperSample <- R6::R6Class(
         metric_obj = self$riem_metric
       )
     },
+
+    #' @description Compute the Frechet mean of the aggregated sample.
+    #'
+    #' @return None. This function is called for its side effects. The result is stored in the `frechet_mean` active binding.
     compute_fmean = function() {
       if (private$gathered_sample |> is.null()) self$gather()
 
       private$gathered_sample$compute_fmean()
       private$f_mean <- private$gathered_sample$frechet_mean
     },
+
+    #' @description Compute the within-group covariance matrix (W) for the samples.
+    #'
+    #' @return None. This function is called for its side effects. The result is stored in the `Within` active binding.
     compute_W = function() {
       private$W <- private$samples |>
         purrr::map(
@@ -104,6 +133,10 @@ CSuperSample <- R6::R6Class(
         _$mat |>
         Matrix::pack()
     },
+
+    #' @description Compute the total covariance matrix (T) for the samples.
+    #'
+    #' @return None. This function is called for its side effects. The result is stored in the `Total` active binding.
     compute_T = function() {
       if (private$gathered_sample |> is.null()) {
         self$gather()
@@ -159,16 +192,37 @@ CSuperSample <- R6::R6Class(
     }
   ),
   active = list(
+    #' @field list_of_samples The list of CSample objects aggregated in this super-sample.
     list_of_samples = function() private$samples,
+
+    #' @field sample_size The total number of connectomes in all samples.
     sample_size = function() private$n,
+
+    #' @field matrix_size The size of the connectome matrices.
     matrix_size = function() private$p,
+
+    #' @field mfd_dim The dimension of the manifold.
     mfd_dim = function() private$d,
+
+    #' @field riem_metric The Riemannian metric used by all samples.
     riem_metric = function() private$geom,
+
+    #' @field variation The total variation of the aggregated sample.
     variation = function() private$var,
+
+    #' @field sample_cov The sample covariance matrix of the aggregated sample.
     sample_cov = function() private$s_cov,
+
+    #' @field full_sample The aggregated CSample object containing all connectomes.
     full_sample = function() private$gathered_sample,
+
+    #' @field frechet_mean The Frechet mean of the aggregated sample.
     frechet_mean = function() private$f_mean,
+
+    #' @field Within The within-group covariance matrix (W).
     Within = function() private$W,
+
+    #' @field Total The total covariance matrix (T).
     Total = function() private$T
   )
 )
